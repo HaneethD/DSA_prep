cc9baba6aebdda7ea113ed3bcd2612c048f662d9
Haneeth Devangam <110095548+HaneethD@users.noreply.github.com>
2022-08-02 10:59:27 -0700

    Create maximum_subarray_sum.cpp

---
+++ DSA/Arrays/maximum_subarray_sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum = 0 ;
+        int maximum = INT_MIN;
+        for(auto it : nums)
+        {
+            sum+=it;
+            maximum = max(sum, maximum);
+            if(sum<0)
+            {
+                sum =0;
+            }
+            
+        }
+        return maximum;
+        
+    }
+};
+//this is the linear time complexity solution
+//Done using KADANE ALGORITHM
